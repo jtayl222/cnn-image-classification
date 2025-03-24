f839a69515cff2c2cfbf5e3726c33aad78308672
jtayl222 <jtayl22,at,gmail,dot,com@obfuscated>
2025-03-24 19:09:29 -0400

    added MLflow

--- train.py
+++ train.py
@@ -11,18 +11,19 @@ from torch import nn, optim
 from torchvision import datasets, transforms
 import argparse
 from load_model import load_model
+import mlflow
+import mlflow.pytorch
+from mlflow.models.signature import infer_signature
+import time
 
-def train(model, train_loader, validation_loader, criterion, optimizer, epochs=5, print_every=40, device="cpu"):
+def train(model, train_loader, validation_loader, criterion, optimizer, scheduler, epochs=5, print_every=40, device="cpu"):
     model.to(device)
-    model.train()
-    steps = 0
-    running_loss = 0
     for epoch in range(epochs):
+        model.train()
+        running_loss = 0
+        start_time = time.time()
         for images, labels in train_loader:
             images, labels = images.to(device), labels.to(device)
-            steps += 1
-            print(steps, len(images.data), len(labels.data))
-
             logps = model.forward(images)
             loss = criterion(logps, labels)
             optimizer.zero_grad()
@@ -30,20 +31,30 @@ def train(model, train_loader, validation_loader, criterion, optimizer, epochs=5
             optimizer.step()
             running_loss += loss.item()
 
-            if steps % print_every == 0:
-                model.eval()
-                validation_loss, accuracy = evaluate(model, validation_loader, criterion, device)
-                print(f"Epoch {epoch + 1}/{epochs}.. "
-                      f"Train loss: {running_loss / print_every:.3f}.. "
-                      f"Validation loss: {validation_loss:.3f}.. "
-                      f"Test accuracy: {accuracy:.3f}")
-                running_loss = 0
-                model.train()
+        model.eval()
+        validation_loss, accuracy, top5_accuracy = evaluate(model, validation_loader, criterion, device)
+        epoch_loss = running_loss / len(train_loader)
+        epoch_time = time.time() - start_time
+        print(f"Epoch {epoch+1}/{epochs}.. "
+              f"Train loss: {epoch_loss:.3f}.. "
+              f"Validation loss: {validation_loss:.3f}.. "
+              f"Test accuracy: {accuracy:.3f}.. "
+              f"Epoch time: {epoch_time:.3f}s")
+        mlflow.log_metric("train_loss", epoch_loss, step=epoch+1)
+        mlflow.log_metric("validation_loss", validation_loss, step=epoch+1)
+        mlflow.log_metric("accuracy", accuracy, step=epoch+1)
+        mlflow.log_metric("epoch_time", epoch_time, step=epoch+1)
+        mlflow.log_metric("top_5_accuracy", top5_accuracy, step=epoch+1)
+        
+        scheduler.step()
+        current_scheduler_step = scheduler.last_epoch
+        mlflow.log_metric("scheduler_step", current_scheduler_step, step=epoch+1)
 
 
 def evaluate(model, dataloader, criterion, device):
     accuracy = 0
     test_loss = 0
+    top5_accuracy = 0
     model.eval()
     model.to(device)
     with torch.no_grad():
@@ -58,8 +69,12 @@ def evaluate(model, dataloader, criterion, device):
             top_p, top_class = ps.topk(1, dim=1)
             equals = top_class == labels.view(*top_class.shape)
             accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+            top_p, top_classes = ps.topk(5, dim=1)
+            matches = top_classes.eq(labels.view(-1, 1))
+            top5_correct = matches.any(dim=1).float().sum().item()
+            top5_accuracy += top5_correct / labels.size(0)
 
-    return test_loss / len(dataloader), accuracy / len(dataloader)
+    return test_loss / len(dataloader), accuracy / len(dataloader), top5_accuracy / len(dataloader)
 
 def save_checkpoint(filepath, model):
     checkpoint = {'class_to_idx': model.class_to_idx,
@@ -90,35 +105,81 @@ def init_data_loading(data_dir):
     validation_loader = torch.utils.data.DataLoader(validation_data, batch_size=64)
     return train_loader, validation_loader
 
+# Create a signature and input example for the model for MLflow logging
+def create_signature(model, validation_loader):
+    images, labels = next(iter(validation_loader))   # get a single batch from validation
+    images = images.to('cpu')                 # ensure it's on CPU for logging
+
+    model.eval()
+    with torch.no_grad():
+        outputs = model(images)               # forward pass on this batch
+
+    # log one sample, you can do something like:
+    single_input_example = images[0].unsqueeze(0)
+    single_output = outputs[0].unsqueeze(0)
+
+    # Convert to NumPy for signature inference
+    signature = infer_signature(
+        single_input_example.numpy(),
+        single_output.numpy()
+    )
+    return signature, single_input_example
+    
 def main(
         data_dir="args.data_directory",
         save_dir="checkpoints",
         arch="vgg16",
-        learning_rate=.003,
-        hidden_units=512,
         epochs=5,
         device="cpu",
         print_every = 40):
+    
+    # Example hyperparameter grid
+    lr_values = [0.0001, 0.001, 0.01]
+    hidden_units_values = [256, 512, 1024]
 
-    train_loader, validation_loader = init_data_loading(data_dir)
-    model = load_model(arch, save_dir, hidden_units, train_loader=train_loader)
-    criterion = nn.NLLLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    train(model, train_loader, validation_loader, criterion, optimizer, epochs=epochs, print_every=print_every, device=device)
+    mlflow.set_tracking_uri("file:./mlruns")
+    mlflow.set_experiment("flower-classifier")
 
-    if save_dir is not None:
-        save_checkpoint(save_dir, model)
+    for learning_rate in lr_values:
+        for hidden_units in hidden_units_values:
+            with mlflow.start_run():
+                mlflow.log_param("data_dir", data_dir)
+                mlflow.log_param("save_dir", save_dir)
+                mlflow.log_param("arch", arch)
+                mlflow.log_param("learning_rate", learning_rate)
+                mlflow.log_param("hidden_units", hidden_units)
+                mlflow.log_param("epochs", epochs)
+                mlflow.log_param("device", device)
+                mlflow.log_param("print_every", print_every)
+
+                train_loader, validation_loader = init_data_loading(data_dir)        
+                model = load_model(arch, save_dir, hidden_units, train_loader=train_loader)
+                criterion = nn.NLLLoss()
+                optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+                scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+
+                train(model, train_loader, validation_loader, criterion, optimizer, scheduler, epochs=epochs, print_every=print_every, device=device)
+
+                signature, single_input_example = create_signature(model, validation_loader)
+                single_input_example_np = single_input_example.numpy()
+                mlflow.pytorch.log_model(
+                    model, 
+                    artifact_path="model",
+                    input_example=single_input_example_np,
+                    signature=signature
+                )
+
+                if save_dir is not None:
+                    save_checkpoint(save_dir, model)
 
 ###
 ### Ref: https://docs.python.org/3/howto/argparse.html
 ###
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Train a new network on a data set")
     parser.add_argument("data_directory", help="Directory containing train/{class}/example", default="flowers")
     parser.add_argument("--save_dir", help="checkpoint file", default=None)
     parser.add_argument("--arch", help="Example: vgg13", default="vgg16")
-    parser.add_argument("--learning_rate", default=0.003, type=float)
-    parser.add_argument("--hidden_units", default=512, type=int)
     parser.add_argument("--epochs", default=5, type=int)
     parser.add_argument("--print_every", default=40, type=int)
     parser.add_argument("--gpu")
@@ -134,8 +195,6 @@ if __name__ == '__main__':
         data_dir=args.data_directory,
         save_dir=args.save_dir,
         arch=args.arch,
-        learning_rate=args.learning_rate,
-        hidden_units=args.hidden_units,
         epochs=args.epochs,
         device=device,
         print_every=args.print_every
